8a25a16c12afa41cfe6125c37b4ff1eca72efe82
Bunyamin Kalkan <bnkalkan41@gmail.com>
2024-05-26 12:32:26 +0300

    feat: prepare is done

---
+++ src/main/java/com/example/eshop/EShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EShopApplication.class, args);
+    }
+
+}
